cd055314577b7370b420103cd72293c0f6758e55
Gnimuc <qiyupei@gmail.com>
2021-05-18 15:38:16 +0900

    Test UUID

--- test/suitesparse_uuid.jl
+++ test/suitesparse_uuid.jl
@@ -1,10 +1,10 @@
 using Pkg
 using Test
 
-@testset "Pkg UUID" begin
+@testset "SuiteSparse UUID" begin
     project_filename = joinpath(dirname(@__DIR__), "Project.toml")
     project = Pkg.TOML.parsefile(project_filename)
     uuid = project["uuid"]
-    correct_uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+    correct_uuid = "4607b0f0-06f3-5cda-b6b1-a6196a1729e9"
     @test uuid == correct_uuid
 end
