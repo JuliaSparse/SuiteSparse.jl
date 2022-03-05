b33eab5e9616ab9dbc527b905a67a495d07596c7
Ian <i.r.butterworth@gmail.com>
2022-03-05 15:21:17 -0500

    move threaded testsets to main process

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,22 +11,30 @@ if Base.USE_GPL_LIBS
     include("linalg_tests.jl")
 
     # Test multithreaded execution
-    let p, cmd = `$(Base.julia_cmd()) --depwarn=error --startup-file=no threads.jl`
+    @testset "threaded SuiteSparse tests" verbose = true begin
+        @testset "threads = $(Threads.nthreads())" begin
+            include("threads.jl")
+        end
         # test both nthreads==1 and nthreads>1. spawn a process to test whichever
         # case we are not running currently.
         other_nthreads = Threads.nthreads() == 1 ? 4 : 1
-        p = run(
-                pipeline(
-                    setenv(
-                        cmd,
-                        "JULIA_NUM_THREADS" => other_nthreads,
-                        dir=@__DIR__()),
-                    stdout = stdout,
-                    stderr = stderr),
-                wait = false)
-        include("threads.jl")
-        if !success(p)
-            error("SuiteSparse threads test failed with nthreads == $other_nthreads")
+        @testset "threads = $other_nthreads" begin
+            let p, cmd = `$(Base.julia_cmd()) --depwarn=error --startup-file=no threads.jl`
+                p = run(
+                        pipeline(
+                            setenv(
+                                cmd,
+                                "JULIA_NUM_THREADS" => other_nthreads,
+                                dir=@__DIR__()),
+                            stdout = stdout,
+                            stderr = stderr),
+                        wait = false)
+                if !success(p)
+                    error("SuiteSparse threads test failed with nthreads == $other_nthreads")
+                else
+                    @test true # mimic the one @test in threads.jl
+                end
+            end
         end
     end
 end
